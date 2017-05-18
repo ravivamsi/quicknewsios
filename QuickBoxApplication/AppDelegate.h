c42eb8e58dd5f8a214d323f967b17106d7feacb4
mediapark <hasae300@gmail.com>
2017-05-18 12:49:59 +0500

    Initial Commit

---
+++ QuickBoxApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuickBoxApplication
+//
+//  Created by Media park  on 18/05/2017.
+//  Copyright © 2017 QuickBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuickBoxApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickBoxApplication
+//
+//  Created by Media park  on 18/05/2017.
+//  Copyright © 2017 QuickBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
